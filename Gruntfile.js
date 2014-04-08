3c23e96deadcf6dbc2230883948f5fa4e414b3b9
David LaPalomento <dlapalomento@gmail.com>
2014-04-08 15:24:34 -0400

    Add qunit tests Pull in a qunit build step and run a simple canary test.

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,10 +3,14 @@ module.exports = function(grunt) {
   grunt.initConfig({
     jshint: {
       files: ['Gruntfile.js', 'src/**/*.js', 'test/**/*.js']
+    },
+    qunit: {
+      files: ['test/**/*.html']
     }
   });
 
   grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-qunit');
 
-  grunt.registerTask('default', ['jshint']);
+  grunt.registerTask('default', ['jshint', 'qunit']);
 };
