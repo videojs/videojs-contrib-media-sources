731fe6defc11f20437bd44bd2578d3e80a1c0527
jrivera <jrivera@brightcove.com>
2015-09-10 14:17:48 -0400

    Add a watch system to automatically build distribution files

--- Gruntfile.js
+++ Gruntfile.js
@@ -10,7 +10,7 @@ module.exports = function(grunt) {
       ' Licensed <%= _.pluck(pkg.licenses, "type").join(", ") %> */\n',
     // Task configuration.
     clean: {
-      files: ['build', 'dist']
+      files: ['tmp', 'dist']
     },
     jshint: {
       files: ['Gruntfile.js', 'src/**/*.js', 'test/**/*.js']
@@ -23,7 +23,7 @@ module.exports = function(grunt) {
     importscripts: {
       build: {
         src: 'src/transmuxer_worker.js',
-        dest: 'build/transmuxer_worker.js'
+        dest: 'tmp/transmuxer_worker.js'
       }
     },
     concat: {
@@ -48,29 +48,19 @@ module.exports = function(grunt) {
     blobify: {
       build: {
         src: 'src/videojs-media-sources.js',
-        dest: 'build/videojs-media-sources.js'
+        dest: 'tmp/videojs-media-sources.js'
       }
     },
     uglify: {
       webworker: {
         src: '<%= importscripts.build.dest %>',
-        dest: 'build/transmuxer_worker.min.js'
+        dest: 'tmp/transmuxer_worker.min.js'
       },
       mediasource: {
         options: {
           banner: '<%= banner %>'
         },
-        src: [
-          'node_modules/mux.js/lib/stream.js',
-          'node_modules/mux.js/lib/exp-golomb.js',
-          'node_modules/mux.js/legacy/flv-tag.js',
-          'node_modules/mux.js/legacy/aac-stream.js',
-          'node_modules/mux.js/legacy/h264-stream.js',
-          'node_modules/mux.js/legacy/metadata-stream.js',
-          'node_modules/mux.js/legacy/h264-extradata.js',
-          'node_modules/mux.js/legacy/segment-parser.js',
-          '<%= blobify.build.dest %>'
-        ],
+        src: '<%=concat.dist.src%>',
         dest: 'dist/videojs-media-sources.min.js'
       }
     },
@@ -80,84 +70,103 @@ module.exports = function(grunt) {
           keepalive: true
         }
       }
+    },
+    watch: {
+      scripts: {
+        files: [
+          'src/**/*.js',
+          'node_modules/mux.js/lib/**/*.js',
+          'node_modules/mux.js/legacy/**/*.js'
+        ],
+        tasks: ['build'],
+        options: {
+          spawn: false,
+        }
+      }
     }
   });
 
-  grunt.task.registerMultiTask('importscripts', 'Inline files references via importScripts in a WebWorker', function(arg1, arg2) {
-    var fs = require('fs');
-    var path = require('path');
-    var falafel = require('falafel');
+  grunt.task.registerMultiTask(
+    'importscripts',
+    'Inline files references via importScripts in a WebWorker',
+    function (arg1, arg2) {
+      var fs = require('fs');
+      var path = require('path');
+      var falafel = require('falafel');
 
-    this.files.forEach(function(f) {
-      var src = f.src[0];
-      // Warn on and remove invalid source files (if nonull was set).
-      if (!grunt.file.exists(src)) {
-        grunt.log.warn('Source file "' + filepath + '" not found.');
-        return;
-      }
-
-      // Don't process directories
-      if (grunt.file.isDir(src)) {
-        return;
-      }
-
-      // Read file source.
-      var srcContents = grunt.file.read(src);
-      var currentPath = path.dirname(src);
-
-      // Process the file looking for `importScripts` calls
-      grunt.file.write(f.dest, falafel(srcContents, function (node) {
-        // Check every function call for one that calls importScripts
-        if (node.type === 'CallExpression' && node.callee.name === 'importScripts') {
-          var fileName = node.arguments[0].value;
-          var inlineSrc = grunt.file.read(path.resolve(currentPath, fileName));
-
-          grunt.log.debug('Inlining importScripts file "' + path.resolve(currentPath, fileName) + '"...');
-
-          // Replace the entire importScripts expression with the source of the file
-          node.update(inlineSrc);
+      this.files.forEach(function(f) {
+        var src = f.src[0];
+        // Warn on and remove invalid source files (if nonull was set).
+        if (!grunt.file.exists(src)) {
+          grunt.log.warn('Source file "' + filepath + '" not found.');
+          return;
         }
-      }).toString());
-    });
-  });
 
-  grunt.task.registerMultiTask('blobify', 'Create a blob-based  WebWorker to inline a source file', function(arg1, arg2) {
-    var fs = require('fs');
-    var path = require('path');
-    var falafel = require('falafel');
-
-    this.files.forEach(function(f) {
-      var src = f.src[0];
-      // Warn on and remove invalid source files (if nonull was set).
-      if (!grunt.file.exists(src)) {
-        grunt.log.warn('Source file "' + filepath + '" not found.');
-        return;
-      }
-
-      // Don't process directories
-      if (grunt.file.isDir(src)) {
-        return;
-      }
-
-      // Read file source.
-      var srcContents = grunt.file.read(src);
-      var currentPath = path.dirname(src);
-
-      // Process the file looking for `importScripts` calls
-      grunt.file.write(f.dest, falafel(srcContents, function (node) {
-        // Check every function call for one that calls importScripts
-        // and only imports the file we are looking for
-        if (node.type === 'NewExpression' &&
-          node.callee.name === 'Worker' &&
-          node.arguments[0].right.value.indexOf('transmuxer_worker') > -1) {
-          var inlineSrc = grunt.file.read(path.resolve(currentPath, '../build/transmuxer_worker.min.js'));
-
-          // Replace the entire importScripts expression with the source of the file
-          node.update('new Worker(URL.createObjectURL(new Blob([' + JSON.stringify(inlineSrc) + '], {type: "application/javascript"})))');
+        // Don't process directories
+        if (grunt.file.isDir(src)) {
+          return;
         }
-      }).toString());
+
+        // Read file source.
+        var srcContents = grunt.file.read(src);
+        var currentPath = path.dirname(src);
+
+        // Process the file looking for `importScripts` calls
+        grunt.file.write(f.dest, falafel(srcContents, function (node) {
+          // Check every function call for one that calls importScripts
+          if (node.type === 'CallExpression' && node.callee.name === 'importScripts') {
+            var fileName = node.arguments[0].value;
+            var inlineSrc = grunt.file.read(path.resolve(currentPath, fileName));
+
+            grunt.log.debug('Inlining importScripts file "' + path.resolve(currentPath, fileName) + '"...');
+
+            // Replace the entire importScripts expression with the source of the file
+            node.update(inlineSrc);
+          }
+        }).toString());
+      });
     });
-  });
+
+  grunt.task.registerMultiTask(
+    'blobify',
+    'Create a blob-based  WebWorker to inline a source file',
+    function (arg1, arg2) {
+      var fs = require('fs');
+      var path = require('path');
+      var falafel = require('falafel');
+
+      this.files.forEach(function(f) {
+        var src = f.src[0];
+        // Warn on and remove invalid source files (if nonull was set).
+        if (!grunt.file.exists(src)) {
+          grunt.log.warn('Source file "' + filepath + '" not found.');
+          return;
+        }
+
+        // Don't process directories
+        if (grunt.file.isDir(src)) {
+          return;
+        }
+
+        // Read file source.
+        var srcContents = grunt.file.read(src);
+        var currentPath = path.dirname(src);
+
+        // Process the file looking for `importScripts` calls
+        grunt.file.write(f.dest, falafel(srcContents, function (node) {
+          // Check every function call for one that calls importScripts
+          // and only imports the file we are looking for
+          if (node.type === 'NewExpression' &&
+            node.callee.name === 'Worker' &&
+            node.arguments[0].right.value.indexOf('transmuxer_worker') > -1) {
+            var inlineSrc = grunt.file.read(path.resolve(currentPath, '../tmp/transmuxer_worker.min.js'));
+
+            // Replace the entire importScripts expression with the source of the file
+            node.update('new Worker(URL.createObjectURL(new Blob([' + JSON.stringify(inlineSrc) + '], {type: "application/javascript"})))');
+          }
+        }).toString());
+      });
+    });
 
   grunt.loadNpmTasks('grunt-contrib-clean');
   grunt.loadNpmTasks('grunt-contrib-concat');
@@ -165,7 +174,10 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-karma');
   grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-watch');
 
-  grunt.registerTask('default', ['jshint', 'karma']);
-  grunt.registerTask('build', ['clean', 'importscripts', 'uglify:webworker', 'blobify', 'uglify:mediasource']);
+  grunt.registerTask('default', ['test', 'build']);
+  grunt.registerTask('test', ['jshint', 'karma']);
+  grunt.registerTask('build', ['clean', 'importscripts', 'uglify:webworker', 'blobify', 'concat', 'uglify:mediasource']);
+  grunt.registerTask('dev', ['build', 'watch']);
 };
