97654daa04fc9e56293e9d832d4c3986af9b6915
brandonocasey <brandonocasey@gmail.com>
2015-12-29 11:32:39 -0500

    example now follows standards and works

--- example/before-processing.js
+++ example/before-processing.js
@@ -1,33 +1,29 @@
-let videojs = require('video.js'),
-let videojs = require('../src/videojs-media-sources.js')(videojs),
-  req = new XMLHttpRequest();
-
+let videojs = require('video.js');
+let req = new XMLHttpRequest();
 // initialize video.js
-video = videojs('video');
+let video = videojs('video');
 
 // the flash-based media sources implementation only supports FLV video data
 // use XMLHttpRequest2 to get the raw byte array of an example FLV
 req.open('GET', 'barsandtone.flv', true);
 req.responseType = 'arraybuffer';
 
-req.onload = function(event){
-  console.log(event);
-  var
-    // create a new media source to hold the data buffers
-    mediaSource = new videojs.MediaSource(),
+req.onload = function(event) {
+  // create a new media source to hold the data buffers
+  /* eslint-disable new-cap */
+  let mediaSource = new videojs.contribMediaSources();
+  /* eslint-enable new-cap */
 
-    // wrap the arraybuffer in a view so we can easily work with the
-    // individual bytes
-    bytes = new Uint8Array(req.response),
-
-    url;
+  // wrap the arraybuffer in a view so we can easily work with the
+  // individual bytes
+  let bytes = new Uint8Array(req.response);
+  let url;
 
   // when a media source is assigned to a video element the `sourceopen`
   // event fires
-  mediaSource.addEventListener('sourceopen', function(event){
-
+  mediaSource.addEventListener('sourceopen', function(e) {
     // construct the video data buffer and set the appropriate MIME type
-    var sourceBuffer = mediaSource.addSourceBuffer('video/flv; codecs="vp6,aac"');
+    let sourceBuffer = mediaSource.addSourceBuffer('video/flv; codecs="vp6,aac"');
 
     // start feeding bytes to the buffer
     // the video element that is reading from the associated media buffer is
@@ -42,7 +38,7 @@ req.onload = function(event){
   // assign the media source URL to video.js
   video.src({
     src: url,
-    type: "video/flv"
+    type: 'video/flv'
   });
 };
 req.send(null);
